1382fcc0e1c8551280fd804f0df9a2e935dce415
Pyrrh1c <pyrrh1c@protonmail.com>
2020-06-24 09:29:18 -0400

    Basic Functionality is Running.

--- Get-MsolMfaStatus.ps1
+++ Get-MsolMfaStatus.ps1
@@ -1,3 +1,8 @@
+#Requires -Module MSOnline
+
 Connect-MsolService
 $Users = Get-MsolUser -All
-$Users | Foreach-Object{Get-MsolUser -UserPrincipalName $_.UserPrincipalName} | Where-Object{$_.StrongAuthenticationMethods -ne $null}
+$Users | `
+Foreach-Object{Get-MsolUser -UserPrincipalName $_.UserPrincipalName} | `
+Where-Object{$_.StrongAuthenticationMethods -ne $null} | `
+Format-Table -Property DisplayName,UserPrincipalName -AutoSize
